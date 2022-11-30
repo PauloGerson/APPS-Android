b0fad1b1255f5a9a11b27a8797223bbbc6bc27c9
PauloGerson <paulogerson81@gmail.com>
2022-11-30 08:33:22 -0300

    ajustando codigo

---
+++ SetColor/app/src/main/java/com/example/setcolor/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.setcolor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    RadioButton red, yellow, blue;
+
+    SharedPreferences preferences;
+
+    View view1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        red = findViewById(R.id.red);
+        yellow = findViewById(R.id.yellow);
+        blue = findViewById(R.id.blue);
+
+        view1 = findViewById(R.id.view);
+
+
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+                view1.setBackgroundResource(R.color.red);
+            }
+        });
+
+        yellow.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+
+                view1.setBackgroundResource(R.color.yellow);
+            }
+        });
+
+        blue.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+                    view1.setBackgroundResource(R.color.blue);
+            }
+        });
+
+
+
+    }
+
+
+}
